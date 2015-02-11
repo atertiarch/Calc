76325d6807cb1d12982f6bc1cc21f6ffe574c2d8
Marcin Godyń <marcin.godyn@schibsted.pl>
2015-02-11 09:38:36 +0100

    Added basic operations tests.

--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,6 +1,11 @@
 package tests;
 
+import java.util.Vector;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
+
+import calculator.Calculator;
 
 public class CalculatorTest {
 
@@ -9,8 +14,39 @@ public class CalculatorTest {
 		//given
 		String equation = "2+2";
 		//when
-		Calculator.
+		Vector<String> calculations = Calculator.calculations(equation);
 		//then
+		assertTrue(4.0 == Double.valueOf(calculations.get(0)));
+	}
+	
+	@Test
+	public void shouldSubstract(){
+		//given
+		String equation = "5-2";
+		//when
+		Vector<String> calculations = Calculator.calculations(equation);
+		//then
+		assertTrue(3.0 == Double.valueOf(calculations.get(0)));
+	}
+	
+	@Test
+	public void shouldMultiply(){
+		//given
+		String equation = "2*3";
+		//when
+		Vector<String> calculations = Calculator.calculations(equation);
+		//then
+		assertTrue(6.0 == Double.valueOf(calculations.get(0)));
+	}
+	
+	@Test
+	public void shouldDivide(){
+		//given
+		String equation = "6/3";
+		//when
+		Vector<String> calculations = Calculator.calculations(equation);
+		//then
+		assertTrue(2.0 == Double.valueOf(calculations.get(0)));
 	}
 	
 }
