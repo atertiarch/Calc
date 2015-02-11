cae47ff5f3d0e45a25e5464d944f64fb45f2328e
Marcin Godyń <marcin.godyn@schibsted.pl>
2015-02-11 09:33:53 +0100

    added tests.

---
+++ tests/CalculatorTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void shouldAdd(){
+		//given
+		String equation = "2+2";
+		//when
+		Calculator.
+		//then
+	}
+	
+}
