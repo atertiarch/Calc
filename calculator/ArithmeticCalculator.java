bf8e39c250c4f77f8e0c3d4d6f9b844d8debc23d
Marcin Godyń <marcin.godyn@schibsted.pl>
2015-04-07 14:31:45 +0200

    Wyciagnalem do osobnych metod kazde dzialanie.

--- calculator/ArithmeticCalculator.java
+++ calculator/ArithmeticCalculator.java
@@ -22,45 +22,85 @@ public class ArithmeticCalculator {
 
 	    while(operators.contains(vSorted.lastElement())){
 	    	if(vSorted.get(i).equals("-")){
-			    c=Double.parseDouble(vSorted.get(i-1));
-			    d=Double.parseDouble(vSorted.get(i-2));
-			    x=d-c;
-			    i = removeValuesFromString(vSorted, i, x);
+			    i = substract(vSorted, i);
 
 			}
 		    else if(vSorted.get(i).equals("+")){
-			    c=Double.parseDouble(vSorted.get(i-1));
-			    d=Double.parseDouble(vSorted.get(i-2));
-			    x=d+c;
-			    i = removeValuesFromString(vSorted, i, x);
+			    i = add(vSorted, i);
 			}
 		    else if(vSorted.get(i).equals("*")){
-		    	c=Double.parseDouble(vSorted.get(i-1));
-		    	d=Double.parseDouble(vSorted.get(i-2));
-		    	x=d*c;
-		    	i = removeValuesFromString(vSorted, i, x);
+		    	i = multiply(vSorted, i);
 		    }
 		    else if(vSorted.get(i).equals("/")){
-		    	c=Double.parseDouble(vSorted.get(i-1));
-		    	d=Double.parseDouble(vSorted.get(i-2));
-		    	if (c!=0)	{
-			    	x=d/c;
-			    	i = removeValuesFromString(vSorted, i, x);
-			    	}
-		    	else	{
-		    		throw new ArithmeticException("Nigdy cholero nie dziel przez 0!");
-		    	}
+		    	i = divide(vSorted, i);
 		    }
 		    else if(vSorted.get(i).equals("^")){
-		    	c=Double.parseDouble(vSorted.get(i-1));
-		    	d=Double.parseDouble(vSorted.get(i-2));
-		    	x=Math.pow(d, c);
-		    	i = removeValuesFromString(vSorted, i, x);
+		    	i = power(vSorted, i);
 		    }
 		    else i++;	
 	    	
 	    }
 		return vSorted;
+	}
+
+	private static int power(Vector<String> vSorted, int i) {
+		double c;
+		double d;
+		double x;
+		c=Double.parseDouble(vSorted.get(i-1));
+		d=Double.parseDouble(vSorted.get(i-2));
+		x=Math.pow(d, c);
+		i = removeValuesFromString(vSorted, i, x);
+		return i;
+	}
+
+	private static int divide(Vector<String> vSorted, int i) {
+		double c;
+		double d;
+		double x;
+		c=Double.parseDouble(vSorted.get(i-1));
+		d=Double.parseDouble(vSorted.get(i-2));
+		if (c!=0)	{
+			x=d/c;
+			i = removeValuesFromString(vSorted, i, x);
+			}
+		else	{
+			throw new ArithmeticException("Nigdy cholero nie dziel przez 0!");
+		}
+		return i;
+	}
+
+	private static int multiply(Vector<String> vSorted, int i) {
+		double c;
+		double d;
+		double x;
+		c=Double.parseDouble(vSorted.get(i-1));
+		d=Double.parseDouble(vSorted.get(i-2));
+		x=d*c;
+		i = removeValuesFromString(vSorted, i, x);
+		return i;
+	}
+
+	private static int add(Vector<String> vSorted, int i) {
+		double c;
+		double d;
+		double x;
+		c=Double.parseDouble(vSorted.get(i-1));
+		d=Double.parseDouble(vSorted.get(i-2));
+		x=d+c;
+		i = removeValuesFromString(vSorted, i, x);
+		return i;
+	}
+
+	private static int substract(Vector<String> vSorted, int i) {
+		double c;
+		double d;
+		double x;
+		c=Double.parseDouble(vSorted.get(i-1));
+		d=Double.parseDouble(vSorted.get(i-2));
+		x=d-c;
+		i = removeValuesFromString(vSorted, i, x);
+		return i;
 	}
 
 	private static int removeValuesFromString(Vector<String> vSorted, int i,
